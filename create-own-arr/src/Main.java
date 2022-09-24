2a4731ed0b097703f184f7d948a9a6a96fb504bc
wyf <1669313568@qq.com>
2022-09-24 11:28:47 +0800

    create-own-arr 练习array写法 2022/9/24

---
+++ create-own-arr/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Array<Integer> arr = new Array<>();
+
+        arr.addFirst(1);
+        arr.addFirst(2);
+        System.out.println(arr);
+    }
+}
